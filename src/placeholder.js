d1dfd92fc05920e42d6fedf0182ba4a003a97e55
Anibal António <djani454@hotmail.com>
2022-03-10 11:24:34 +0100

    refactor(Previewer): :speech_balloon:

--- src/placeholder.js
+++ src/placeholder.js
@@ -5,8 +5,40 @@ const placeholder = `
 
   ### Markdown Cheat Sheet
 
+  \`
+    // using const  in js
+    const name="Markdown"
+    console.log(name)
+  \`
+
+  \`\`\`
+  /*
+   Log the result in console
+  */
+  const name="Markdown"
+  console.log(name)
+\`\`\`
+
+
+  #### Skils: 
+
+  1. Javascript/Typescrypt
+  2. reactjs
+  3. Angular
+  4. PLSQL 
+  5. HTML
+  6. CSS
+
+  
+  > *When I code, I feel like **super hero** with super powers to transform code into something useful* - [@anibal.antonio](https://www.linkedin.com/in/anibal-antonio)
+
+
+
+
   A quick reference to the Markdown syntax.
-  [Markdown Cheat Sheet](https://www.markdownguide.org/cheat-sheet/#extended-syntax)
+[Markdown Cheat Sheet](https://www.markdownguide.org/cheat-sheet/#extended-syntax)
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
 `;
 
 export default placeholder;
